082a39c023b979aace9b9703c25241b62cf16e45
T3tje <tilman2013@gmail.com>
2023-12-27 13:02:44 +0100

    add an exampleTest

--- backend/src/test/java/com/tuneflux/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/tuneflux/backend/BackendApplicationTests.java
@@ -3,9 +3,17 @@ package com.tuneflux.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+	@Test
+			void exampleTest() {
+		boolean expected = true;
+		boolean actual = true;
+
+		assertEquals(actual, expected);
+	}
 
 }
