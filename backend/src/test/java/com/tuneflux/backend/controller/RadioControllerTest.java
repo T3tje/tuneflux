b7934b14994eecd385c675b174906ffe07161d27
T3tje <tilman2013@gmail.com>
2024-01-09 08:58:02 +0100

    add RadioControllerTest - test a correct request

--- backend/src/test/java/com/tuneflux/backend/controller/RadioControllerTest.java
+++ backend/src/test/java/com/tuneflux/backend/controller/RadioControllerTest.java
@@ -8,15 +8,15 @@ import java.util.Collections;
 
 class RadioControllerTest {
 
+    // Create a mock instance of RadioService
+    RadioService radioService = mock(RadioService.class);
+
+    // Create an instance of RadioController using the mock RadioService
+    RadioController radioController = new RadioController(radioService);
+
     @Test
     void getRadioStations_shouldCallServiceFunctionGetRadioStationsWithStandardparameters_whenRequestIsEmpty() {
         // GIVEN
-        // Create a mock instance of RadioService
-        RadioService radioService = mock(RadioService.class);
-
-        // Create an instance of RadioController using the mock RadioService
-        RadioController radioController = new RadioController(radioService);
-
         // Define the behavior of the mock RadioService:
         // When getRadioStations is called with any String parameter, then return an empty list
         when(radioService.getRadioStations(anyString())).thenReturn(Collections.emptyList());
@@ -30,5 +30,22 @@ class RadioControllerTest {
         // Verify that the getRadioStations method on the mock RadioService was called
         // with the expected URL parameter
         verify(radioService).getRadioStations("https://de1.api.radio-browser.info/json/stations/search?limit=10&reverse=true&order=votes&offset=0&tagList=&name=&country=");
+    }
+    @Test
+    void getRadioStations_shouldCallServiceFunctionGetRadioStationsWithCorrectUri_whenRequestIsCorrect() {
+        // GIVEN
+        // Define the behavior of the mock RadioService:
+        // When getRadioStations is called with any String parameter, then return an empty list
+        when(radioService.getRadioStations(anyString())).thenReturn(Collections.emptyList());
+
+        // WHEN
+        // Call the getRadioStations method on the RadioController
+        // This will internally call the mocked RadioService
+        radioController.getRadioStations(20, "false", "clickcount", 20, "jazz,cool jazz", "", "The United States Of America");
+
+        // THEN
+        // Verify that the getRadioStations method on the mock RadioService was called
+        // with the expected URL parameter
+        verify(radioService).getRadioStations("https://de1.api.radio-browser.info/json/stations/search?limit=20&reverse=false&order=clickcount&offset=20&tagList=jazz,cool%20jazz&name=&country=The%20United%20States%20Of%20America");
     }
 }
