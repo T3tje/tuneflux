88503b15d429a8dd1bef9709e3e120e826e6661b
T3tje <tilman2013@gmail.com>
2024-01-11 11:32:50 +0100

    add AudioPlayer

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,15 @@
 import '../stylesheets/App.css';
 import Header from './Header.tsx';
 import List from './List.tsx';
-import { useState, useEffect } from 'react';
+import {useState, useEffect, useRef} from 'react';
 import axios from 'axios'; // Stelle sicher, dass Axios installiert ist: npm install axios
 import RadioStation from '../models/RadioStation.ts';
 
+
 function App() {
     const [mainList, setMainList] = useState<RadioStation[]>([]);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef<HTMLAudioElement>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,10 +25,32 @@ function App() {
         fetchData();
     }, []); // Leerer Dependency Array, um sicherzustellen, dass der Effekt nur einmal bei der Montage aufgerufen wird
 
+    const togglePlay = () => {
+        const audioElement = audioRef.current;
+
+        if (audioElement) {
+            if (isPlaying) {
+                audioElement.pause();
+            } else {
+                audioElement.play();
+            }
+
+            setIsPlaying((prevIsPlaying) => !prevIsPlaying);
+        }
+    };
+
     return (
         <>
             <Header />
             <List mainList={mainList} />
+
+            <div id="audioPlayer">
+                <button id="playButton" onClick={togglePlay}>
+                    {isPlaying ? 'Pause' : 'Play'}
+                </button>
+                <audio ref={audioRef} src="http://stream.dancewave.online:8080/dance.mp3" />
+            </div>
+
         </>
     );
 }
--- frontend/src/components/RadioStationItem.tsx
+++ frontend/src/components/RadioStationItem.tsx
@@ -1,11 +1,24 @@
 import RadioStation from "../models/RadioStation.ts";
+import "../stylesheets/RadioStationItem.css"
 
 
 type RadioStationItemProps = {
     radioStation: RadioStation
 }
 export default function RadioStationItem(props: Readonly<RadioStationItemProps>) {
+
     return(
-            <li>{props.radioStation.name}</li>
+        <li className="stationListItem">
+            <div>{props.radioStation.name}</div>
+            <div className="buttonDiv">
+                {props.radioStation.name ?
+                    <button className="heartButton emptyHeart">♡</button> :
+                    <button className="heartButton fullHeart">♥</button>
+                }
+                <button className="playButton">▶</button>
+            </div>
+        </li>
+
+
     )
 }
